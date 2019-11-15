59140980db3d9dc8fc27e027858af5414f8173c9
Evgeny Gushchin <latenal@gmail.com>
2019-11-14 23:46:08 -0600

    added main service test

--- projects/angular-rest/src/lib/angular-rest.service.spec.ts
+++ projects/angular-rest/src/lib/angular-rest.service.spec.ts
@@ -1,9 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CookieModule, CookieService, COOKIE_OPTIONS } from 'ngx-cookie';
+
+
 
 import { RestClientService } from './angular-rest.service';
 
-describe('AngularRestService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('RestClientService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+      CookieModule.forRoot(),
+      RouterTestingModule
+    ],
+    providers: [RestClientService, CookieService]
+  }));
 
   it('should be created', () => {
     const service: RestClientService = TestBed.get(RestClientService);
